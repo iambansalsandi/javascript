88e67aac2d2f736935532d6da788c9d851f254eb
iambansalsandi <isandeepbansal4o4@gmail.com>
2019-03-26 22:21:15 +0530

    strings in javascript :smile:

--- 01_basics/data_types.js
+++ 01_basics/data_types.js
@@ -7,3 +7,12 @@ let rollNumber = 786512;
 const PI = 3.14;
 
 // check scope of let, var and const in details
+
+// Strings
+let firstName = 'Sandeep';
+let lastName = 'Bansal';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log(fullName.length); // String length
+console.log(fullName[0]); // First letter of string
+console.log(fullName[fullName.length - 1]); // Last letter of a string
